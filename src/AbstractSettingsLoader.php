c34fcc952753542e0e9151cfdefeda0d91e3f6e5
Eric Chau <eriic.chau@gmail.com>
2017-06-26 21:29:07 +0000

    updated AbstractSettingsLoader to support env

--- src/AbstractSettingsLoader.php
+++ src/AbstractSettingsLoader.php
@@ -10,20 +10,29 @@ namespace Jarvis\Skill\Helper;
 abstract class AbstractSettingsLoader
 {
     /**
-     * Returns an array that contains current project global settings.
+     * Returns an array that contains current project global settings. If an env
+     * is given, it will try to load the file and override values.
      *
      * @return array
      */
-    public static function load(): array
+    public static function load(string $env = ''): array
     {
-        $vars = static::vars();
-        $raw = str_replace(
-            array_keys($vars),
-            array_values($vars),
-            file_get_contents(static::settingsPath())
-        );
+        $filepath = static::settingsPath();
+        if (false === $filepath) {
+            throw new \RuntimeException(
+                'Failed to read app_settings.json, the file does not exist or is not readable.'
+            );
+        }
 
-        return json_decode($raw, true);
+        $settings = static::read($filepath);
+        if (false != $env && false !== $envFilepath = static::settingsPath($env)) {
+            $settings = array_merge_assoc_recursive(
+                $settings,
+                static::read($envFilepath)
+            );
+        }
+
+        return $settings;
     }
 
     /**
@@ -32,16 +41,6 @@ abstract class AbstractSettingsLoader
      * @return string
      */
     abstract protected static function rootdir(): string;
-
-    /**
-     * Returns project global settings file path.
-     *
-     * @return string
-     */
-    protected static function settingsPath(): string
-    {
-        return realpath(static::rootdir() . '/res/app_settings.json');
-    }
 
     /**
      * Returns list of variables to replace in global settings raw string.
@@ -53,5 +52,50 @@ abstract class AbstractSettingsLoader
         return [
             '%root_dir%' => static::rootdir(),
         ];
+    }
+
+    /**
+     * Returns project global settings file path according to provided environment.
+     *
+     * @param string $env The environment to use
+     *
+     * @return string
+     */
+    private static function settingsPath(string $env = ''): string
+    {
+        return realpath(sprintf(
+            '%s/res/app_settings%s.json',
+            static::rootdir(),
+            $env ? '.' . $env : ''
+        ));
+    }
+
+    /**
+     * Reads the provided JSON file path, computes it and returns it as array.
+     *
+     * @param  string $path The JSON file path to read
+     *
+     * @return array the array that contains data of provided JSON file
+     */
+    private static function read(string $path): array
+    {
+        $vars = static::vars();
+        $raw = str_replace(
+            array_keys($vars),
+            array_values($vars),
+            file_get_contents($path)
+        );
+
+        $result = json_decode($raw, true);
+        if (JSON_ERROR_NONE !== json_last_error()) {
+            throw new \InvalidArgumentException(sprintf(
+                'Failed to read JSON file %s, an error occured: %s (%d)',
+                $path,
+                json_last_error_msg(),
+                json_last_error()
+            ));
+        }
+
+        return $result;
     }
 }
